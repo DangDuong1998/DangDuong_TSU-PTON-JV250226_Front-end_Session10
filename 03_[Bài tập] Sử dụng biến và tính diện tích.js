22aec852f4a37e79bab11851d37208dc02389bc5
DangDuong1998 <mvstarkfinal.ultra@gmail.com>
2025-03-24 17:19:59 +0900

    upload 03_[Bài tập] Sử dụng biến và tính diện tích (html+js)

---
+++ 03_[Bài tập] Sử dụng biến và tính diện tích.js
@@ -0,0 +1,5 @@
+let width = Number(prompt("Nhập chiều rộng: "));
+let height = Number(prompt("Nhập chiều dài: "));
+let area = width * height;
+document.write("Diện tích hình chữ nhật sẽ là: " + area);
+console.log(area);
